2439609d83a67c58b17e738f822b6a0916b0a9ff
schilling-p <pschilli97@googlemail.com>
2021-03-29 17:27:25 +0200

    changed the greeting

--- sample_website/website.py
+++ sample_website/website.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, Class!</h1>'
+    return '<h1>Hello, wonderful people out there!</h1>'
 
 
 if __name__ == "__main__":
